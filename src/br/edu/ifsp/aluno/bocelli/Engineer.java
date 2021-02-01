1539205d1a3697305f5a8636c1eb0384cd0499bc
Júnior Bocelli <juniorbocelli@gmail.com>
2021-02-01 17:09:39 -0300

    Interface funcional, navegação e criação de mecânicos.

--- src/br/edu/ifsp/aluno/bocelli/Engineer.java
+++ src/br/edu/ifsp/aluno/bocelli/Engineer.java
@@ -1,4 +1,17 @@
 package br.edu.ifsp.aluno.bocelli;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.ArrayList;
+
 public class Engineer {
+    private WebDriver webDriver;
+
+    public Engineer(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void tryCreate(String cpf, String nome, String nascimento, String sexo, String salario, ArrayList<String> telefones, ArrayList<String> emails) {
+
+    }
 }
