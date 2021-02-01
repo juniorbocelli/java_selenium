0f80b75c7b3bd2dd9ae6861b20f553f1f5e933ce
Júnior Bocelli <juniorbocelli@gmail.com>
2021-01-31 23:04:27 -0300

    Início do sistema

---
+++ src/br/edu/ifsp/aluno/bocelli/Main.java
@@ -0,0 +1,28 @@
+package br.edu.ifsp.aluno.bocelli;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        // TODO Auto-generated method stub
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\junio\\IdeaProjects\\TPC1_Prova_2\\lib\\chrome\\chromedriver.exe");
+
+        // Funciona como se fosse a instância do navegador
+        WebDriver webDriver = new ChromeDriver();
+
+        // Abre o navegador e entra em página
+        webDriver.get("https://google.com");
+
+        // Maximiza navegador
+        webDriver.manage().window().maximize();
+
+        // Espera milisegundos
+        Thread.sleep(5000);
+
+        // Fecha o navegador (clse() fecha apenas a aba)
+        webDriver.quit();
+    }
+
+}
