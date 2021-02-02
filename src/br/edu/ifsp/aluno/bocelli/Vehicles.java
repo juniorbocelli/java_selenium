28b859c520eacc4d15201beeb270a32617f8822b
Júnior Bocelli <juniorbocelli@gmail.com>
2021-02-02 10:08:04 -0300

    Melhorou organização das classes.

--- src/br/edu/ifsp/aluno/bocelli/Vehicles.java
+++ src/br/edu/ifsp/aluno/bocelli/Vehicles.java
@@ -4,60 +4,85 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Vehicles {
+public class Vehicle {
     private WebDriver webDriver;
     private ErrorHandler errorHandler;
     private Navigation navigation;
 
-    public Vehicles(WebDriver webDriver) {
+    public Vehicle(WebDriver webDriver) {
         this.webDriver = webDriver;
         this.errorHandler = new ErrorHandler(this.webDriver);
         this.navigation = new Navigation(this.webDriver);
     }
 
-    public void tryCreate(String cpf, String nome, String nascimento, String sexo, String salario, String[] telefones, String[] emails) throws InterruptedException {
+    public void tryCreate(String placaLetras, String placaNumeros, String placaEstado, String placaCidade, String tipo, String marca, String modelo, String ano, String portas, String lugares, String combustivel, String cor, String[] acessorios) throws InterruptedException {
         // Verifica se está na pagina correta
-        if(webDriver.getTitle().compareTo("Oficina Mecânica - Salvar Mecânico") != 0) {
-            navigation.navigateToEngineersList();
+        if(webDriver.getTitle().compareTo("Oficina Mecânica - Salvar Veículo") != 0) {
+            navigation.navigateToNewVehicle();
         }
 
-        webDriver.findElement(By.id("cpf")).sendKeys(cpf);
-        webDriver.findElement(By.id("nome")).sendKeys(nome);
-        webDriver.findElement(By.id("nascimento")).sendKeys(nascimento);
+        // Letras
+        webDriver.findElement(By.id("placaLetras")).sendKeys(placaLetras);
 
-        List<WebElement> radioElements = new ArrayList<>(webDriver.findElements(By.name("sexo")));
+        // Números
+        webDriver.findElement(By.id("placaNumeros")).sendKeys(placaNumeros);
 
-        for(WebElement element : radioElements) {
-            if(element.getAttribute("value").compareTo(sexo) == 0) {
-                JavascriptExecutor javascriptExecutor = (JavascriptExecutor)webDriver;
-                javascriptExecutor.executeScript("arguments[0].click();", element);
-            }
+        // Estado
+        WebElement dropDown1 = webDriver.findElement(By.id("placaEstado"));
+        Select placaEstadoSelect = new Select(dropDown1);
+        placaEstadoSelect.selectByValue("placaEstado");
+
+        // Cidade
+        WebElement dropDown2 = webDriver.findElement(By.id("placaCidade"));
+        Select placaCidadeSelect = new Select(dropDown2);
+        placaCidadeSelect.selectByValue("placaCidade");
+
+        // Tipo
+        WebElement dropDown3 = webDriver.findElement(By.id("tipo"));
+        Select tipoSelect = new Select(dropDown3);
+        tipoSelect.selectByValue("tipo");
+
+        // Marca
+        WebElement dropDown4 = webDriver.findElement(By.id("marca"));
+        Select marcaSelect = new Select(dropDown4);
+        marcaSelect.selectByValue("marca");
+
+        // Modelo
+        WebElement dropDown5 = webDriver.findElement(By.id("modelo"));
+        Select modeloSelect = new Select(dropDown5);
+        modeloSelect.selectByValue("modelo");
+
+        // Ano
+        webDriver.findElement(By.id("ano")).sendKeys(ano);
+
+        // Portas
+        webDriver.findElement(By.id("portas")).sendKeys(portas);
+
+        // Lugares
+        webDriver.findElement(By.id("lugares")).sendKeys(lugares);
+
+        // Combustível
+        WebElement dropDown6 = webDriver.findElement(By.id("combustivel"));
+        Select combustivelSelect = new Select(dropDown6);
+        combustivelSelect.selectByValue("combustivel");
+
+        // Cor
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor)webDriver;
+        WebElement corElement = webDriver.findElement(By.id("cor"));
+        javascriptExecutor.executeScript("arguments[0].setAttribute('value', '" + cor +"')", corElement);
+
+        // Acessórios
+        if(acessorios.length > 0) {
+            webDriver.findElements(By.name("acessorio")).get(0).sendKeys(acessorios[0]);
         }
-
-        webDriver.findElement(By.id("salario")).sendKeys(salario);
-
-        // Telefones
-        if(telefones.length > 0) {
-            webDriver.findElements(By.name("telefone")).get(0).sendKeys(telefones[0]);
-        }
-
-        for(int i = 1; i < telefones.length; i++) {
-            webDriver.findElement(By.id("novoTelefone")).click();
-            webDriver.findElements(By.name("telefone")).get(i).sendKeys(telefones[i]);
-        }
-
-        // E-mails
-        if(emails.length > 0) {
-            webDriver.findElements(By.name("email")).get(0).sendKeys(emails[0]);
-        }
-
-        for(int i = 1; i < emails.length; i++) {
-            webDriver.findElement(By.id("novoEmail")).click();
-            webDriver.findElements(By.name("email")).get(i).sendKeys(emails[i]);
+        for(int i = 1; i < acessorios.length; i++) {
+            webDriver.findElement(By.id("novoAcessorio")).click();
+            webDriver.findElements(By.name("acessorio")).get(i).sendKeys(acessorios[i]);
         }
 
         // Tenta enviar form
@@ -75,10 +100,10 @@ public class Vehicles {
         }
     }
 
-    public void tryEdit(String index, String cpf, String nome, String nascimento, String sexo, String salario, String[] telefones, String[] emails) throws InterruptedException {
+    public void tryEdit(String index, String placaLetras, String placaNumeros, String placaEstado, String placaCidade, String tipo, String marca, String modelo, String ano, String portas, String lugares, String combustivel, String cor, String[] acessorios) throws InterruptedException {
         // Verifica se está na pagina correta
-        if(webDriver.getCurrentUrl().compareTo("http://localhost/oficina/mecanicos.html") != 0) {
-            navigation.navigateToEngineersList();
+        if(webDriver.getCurrentUrl().compareTo("http://localhost/oficina/veiculos.html") != 0) {
+            navigation.navigateToVehiclesList();
         }
 
         // Tenta selecionar a linha
@@ -90,20 +115,20 @@ public class Vehicles {
             System.out.println("    - Não existe o ítem requerido");
         }
 
-        if(webDriver.getCurrentUrl().compareTo("http://localhost/oficina/novo-mecanico.html?a=e&id=" + index) != 0) {
+        if(webDriver.getCurrentUrl().compareTo("http://localhost/oficina/novo-veiculo.html?a=e&id=" + index) != 0) {
             System.out.println("Foi encontrado um erro:");
             System.out.println("    - A url " + webDriver.getCurrentUrl() + " não é a esperada!");
 
             webDriver.quit();
         }
 
-        tryCreate(cpf, nome, nascimento, sexo, salario, telefones, emails);
+        tryCreate(placaLetras, placaNumeros, placaEstado, placaCidade, tipo, marca, modelo, ano, portas, lugares, combustivel, cor, acessorios);
     }
 
     public void tryDelete(String index) {
         // Verifica se está na pagina correta
-        if(webDriver.getCurrentUrl().compareTo("http://localhost/oficina/mecanicos.html") != 0) {
-            navigation.navigateToEngineersList();
+        if(webDriver.getCurrentUrl().compareTo("http://localhost/oficina/veiculos.html") != 0) {
+            navigation.navigateToVehiclesList();
         }
 
         // Tenta selecionar a linha
@@ -113,6 +138,8 @@ public class Vehicles {
         } else {
             System.out.println("Foi encontrado um erro:");
             System.out.println("    - Não existe o ítem requerido");
+
+            webDriver.quit();
         }
     }
 }
